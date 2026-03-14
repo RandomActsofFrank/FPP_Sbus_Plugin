1061dd073601e18c2b5f90c4bf11d4dce4b60dad
Just Frank <44183481+RandomActsofFrank@users.noreply.github.com>
2026-03-13 22:51:22 -0700

    Add files via upload

--- api.php
+++ api.php
@@ -8,12 +8,10 @@ header('Content-Type: application/json');
 $pluginDir = dirname(__DIR__);
 $configFile = $pluginDir . '/sbus_config.json';
 
-if (!file_exists($configFile)) {
-    echo json_encode(['error' => 'Config not found']);
-    exit;
+$config = [];
+if (file_exists($configFile)) {
+    $config = json_decode(file_get_contents($configFile), true) ?: [];
 }
-
-$config = json_decode(file_get_contents($configFile), true);
 $pidFile = $pluginDir . '/sbus_daemon.pid';
 $running = false;
 if (file_exists($pidFile)) {
